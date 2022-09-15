0eeab357276c18195c422ae41693dcc5ef67440a
RahhalElmaslohi <rahhalelmaslohi@gmail.com>
2022-09-15 11:27:24 -0400

    ajout d'un style

--- function.php
+++ function.php
@@ -46,7 +46,15 @@ function underscore_setup() {
      * Enqueue scripts and styles.
      */
     function underscore_scripts() {
-    	wp_enqueue_style( 'underscore-style', get_stylesheet_uri(), array(), _S_VERSION );
+    	wp_enqueue_style( 'underscore-style',
+		                   get_stylesheet_uri(),
+		                    array(),
+		                     _S_VERSION );
+		wp_enqueue_style('main-styles',
+		                get_template_directory_uri() . '/css/style.css',
+		                 array(),
+		                 filemtime(get_template_directory() . '/css/style.css'), false);
+
     }
     
 }
